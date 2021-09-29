1a41da0a298b5e9182bc93183ae1f5a7179eba9f
Ganwa Khan <ganwahc@gmail.com>
2021-09-29 16:34:09 +0500

    Module B class 1

---
+++ web.js
@@ -0,0 +1,85 @@
+/////************************* Module B Class 1st *************************//////
+
+// // var name = "iffa";
+// // const address = "Gulshan e Iqbal";
+// // let subject = "English";
+
+// {
+//     var name = "iffa";
+//     const address = "Gulshan e Iqbal";
+//     let subject = "English";
+// }
+
+// var i = 100;
+// i = 100+1;
+// console.log(i);
+
+// //why use let
+// for(let i = 0; i <5; i++){
+//     console.log(i);// 0 1 2 4
+// }
+// console.log(i);/// 101
+// const companyName = "shan";
+// console.log(companyName);
+
+// const pi = 3.14;// typeError
+// // pi = 3.89084;
+
+// let city = "Karachi";
+
+// {
+//   const name1 ="iffa";
+//   console.log(name1);
+//   console.log(city);
+// }
+// //console.log(name1);//ReferenceError
+// let x = 1;
+// if (x===1) {
+//     let x =2;
+//     console.log(x);
+// }
+// console.log(x);
+
+// const number = 50;
+// number = 100;
+// console.log(number);// typeError
+// myName = 'Ghanwa';
+// let myName;
+// console.log(myName); //ReferenceError
+ 
+// function age() {
+//     let yourAge = 18;
+//     let name = prompt("enter your age");
+//     if (name >= yourAge) {
+//         document.write("you're eligible");
+//     }else{
+//         document.write("you're not eligible")
+//     }
+// }
+// age();
+// localStorage.setItem("name","Ghanwa");
+// localStorage.setItem("role","Student");
+// console.log(localStorage.getItem("name"));
+// console.log(localStorage.getItem("role"))
+// sessionStorage.removeItem("name");
+// sessionStorage.removeItem("role")
+
+//session Storage
+// let loginInfo ={
+//     userName:"Ghanwa",
+//     isActive:true,
+//     role:"admin"
+
+// }
+// sessionStorage.setItem("auth", JSON.stringify(loginInfo));
+// let myVal = JSON.parse(sessionStorage.getItem("auth"));
+// console.log(myVal.isActive);
+// let info = {
+//     name:"ekeke",
+
+// }
+
+
+
+
+
